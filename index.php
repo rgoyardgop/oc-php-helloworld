1da9981c8978ec054210278adbe9fb27be18a388
Rémi Goyard <r.goyard@groupeonepoint.com>
2018-07-18 08:28:53 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello <?php echo 'php'; ?></title>
+</head>
+<body>
+	<h1><?php echo "Hello Wolrd"; ?></h1>
+	<p>
+		<?php echo 'Version PHP courante : ' . phpversion();?>
+	</p>
+</body>
+</html>
