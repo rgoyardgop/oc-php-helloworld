3b673a1492db60e6c2155b925b86aa3fd6e17daf
Rémi Goyard <r.goyard@groupeonepoint.com>
2018-07-20 09:34:37 +0700

    Add HTML around MySQL

--- mysql.php
+++ mysql.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Hello <?php echo 'php'; ?></title>
+</head>
+<body>
+	<h1><?php echo "MySQL"; ?></h1>
+	
 <?php
 $username = getenv("MYSQL_USER")
 $password = getenv("MYSQL_PWD");
@@ -20,3 +29,5 @@ while ($row = mysql_fetch_array($result)) {
 }
 mysql_close($dbhandle);
 ?>
+</body>
+</html>
